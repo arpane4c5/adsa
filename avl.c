0791ac5c18294bb16bc7518f96ececbae788b04f
arpane4c5 <arpan.e4c5@gmail.com>
2019-09-09 18:08:33 +0530

    AVL all operations.

--- avl.c
+++ avl.c
@@ -13,6 +13,8 @@ node *insert(node *, int);
 node *delete(node *, int);
 void preorder(node *);
 void inorder(node *);
+void display_level(node *, int);
+void level_order(node *);
 int height(node *);
 node *rotate_right(node *);
 node *rotate_left(node *);
@@ -21,6 +23,7 @@ node * LL(node *);
 node * RL(node *);
 node * LR(node *);
 int balance_factor(node *);
+node * successor(node *);
 
 
 
@@ -31,8 +34,8 @@ int main()
 
 	do 
 	{
-		printf("AVL Operations\n");
-		printf("1. Create\n2. Insert \n3. Delete \n4. Display Preorder\n5. Display Inorder\n6. Exit \n");
+		printf("\n*********AVL Operations***********\n");
+		printf("1. Create\n2. Insert \n3. Delete \n4. Display Preorder\n5. Display Inorder\n6. Display Level Order\n7. Exit \n");
 		printf("Enter you choice :: ");
 		scanf("%d", &choice);
 
@@ -47,6 +50,8 @@ int main()
 					{
 						scanf("%d", &x);
 						root = insert(root , x);
+						printf("\nLevel Order : \n");
+						level_order(root);
 					}
 					break;
 
@@ -73,15 +78,20 @@ int main()
 					break;
 
 			case 6:
+					printf("\nDisplay level order :: \n");
+					level_order(root);
+					break;
+
+			case 7:
 					break;
 
 			default:
-					printf("\nInvalid option\n" );
+					printf("\nInvalid option.\n" );
 					break;
 
 		}
 	}
-	while(choice != 6);
+	while(choice != 7);
 	return 0;
 }
 
@@ -106,9 +116,9 @@ node *insert(node *root, int data)
 	{
 		root -> left = insert(root -> left, data);
 		// check for imbalance
-		if (balance_factor(root) == 2)
+		if (balance_factor(root) == 2)   // imbalance does not propagate upwards. Only once needed
 			// if added in the left subtree of root
-			if(data < root -> data)		
+			if(data < root -> left -> data)		
 				root = LL(root);
 			else
 				root = LR(root);
@@ -117,7 +127,7 @@ node *insert(node *root, int data)
 	{
 		root -> right = insert(root -> right, data);
 		if (balance_factor(root) == -2)
-			if(data > root -> data)
+			if(data > root -> right -> data)
 				root = RR(root);
 			else
 				root = RL(root);
@@ -174,7 +184,6 @@ node * rotate_left(node *p)
 	p -> ht = height(p);
 	q -> ht = height(q);
 	return (q);
-
 }
 
 // Calculate balance factor for the current node
@@ -194,28 +203,6 @@ int balance_factor(node *p)
 		right_ht = p -> right -> ht + 1;
 
 	return (left_ht - right_ht);
-}
-
-// Inorder traversal
-void inorder(node *root)
-{
-	if (root != NULL)
-	{
-		inorder(root -> left);
-		printf("%d\t", root->data);
-		inorder(root -> right);
-	}
-}
-
-// Inorder traversal
-void preorder(node *root)
-{
-	if (root != NULL)
-	{
-		printf("%d\t", root->data);
-		preorder(root -> left);
-		preorder(root -> right);
-	}
 }
 
 
@@ -241,7 +228,133 @@ int height(node *p)
 }
 
 
-node * delete(node * root, int data)
+node *delete(node * root, int data)
 {
-	//Find the element in the AVL tree and delete
+	if (root == NULL)
+	{
+		printf("Value not found !!\n");
+		return NULL;
+	}
+	if (data < root -> data)
+	{
+		root  -> left = delete(root -> left, data);
+	}
+	else if(data > root -> data)
+	{
+		root -> right = delete(root -> right, data);
+	}
+	else 	// Value found
+	{
+		// If target node is leaf node
+		if (root -> left == NULL && root -> right == NULL)
+		{
+			node * p = root;
+			free(p);
+			return NULL;
+		}
+		// If target node has single child
+		else if(root -> left == NULL || root -> right == NULL)  
+		{
+			node * p;
+			if (root -> left != NULL)  // if left child exists
+				p = root -> left;
+			else
+				p = root -> right;
+			free (root);
+			return p;
+		}
+		// If target node has both children
+		else
+		{
+			node * p = successor(root);
+			root -> data = p -> data; 	// Overwrite data with inorder successor data
+			root -> right = delete(root->right, p->data);
+		}
+	}
+
+	// Rotate after deletion
+	if (balance_factor(root) == -2)   // if deleted from left subtree
+	{
+		if(balance_factor(root -> right) <= 0)  // if right subtree is right heavy or balanced
+			root = RR(root);
+		else   		// if right subtree is left heavy
+			root = RL(root);
+	}
+	else if (balance_factor(root) == 2) 
+	{
+		// if deleted from right subtree
+		if(balance_factor(root -> left) >= 0)	//if left subtree is left heavy or balanced
+			root = LL(root);
+		else   		//if left subtree is right heavy
+			root = LR(root);
+	}
+
+	root -> ht = height(root);
+	return root;
+}
+
+// Inorder traversal
+void inorder(node *root)
+{
+	if (root != NULL)
+	{
+		inorder(root -> left);
+		printf("%d\t", root->data);
+		inorder(root -> right);
+	}
+}
+
+// Inorder traversal
+void preorder(node *root)
+{
+	if (root != NULL)
+	{
+		printf("%d\t", root->data);
+		preorder(root -> left);
+		preorder(root -> right);
+	}
+}
+
+// Level Order traversal
+void level_order(node *root)
+{
+	if(root == NULL)
+		return;
+	
+	for(int h=0; h <= root->ht; h++)
+	{
+		display_level(root, h);
+		printf("\n");
+	}
+}
+
+node* successor(node * root)
+{
+	if(root == NULL)
+		return NULL;
+	else if(root -> right == NULL)
+		return NULL;
+
+	node * p = root -> right;
+	while(p->left != NULL)
+		p = p -> left;
+
+	return p;
+}
+
+void display_level(node *root, int curr_ht)
+{
+
+	if (root == NULL)
+		return;
+
+	if (curr_ht == 0)
+	{
+		printf("%d\t", root -> data);
+	}
+	else if (curr_ht > 0)
+	{
+		display_level(root -> left, curr_ht-1);
+		display_level(root -> right, curr_ht-1);
+	}
 }
